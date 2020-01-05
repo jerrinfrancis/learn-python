436b6d08c334c7fb4240482ec97bd0f77147dbb4
Jerrin Francis <jerrinfrancis7@gmail.com>
2020-01-05 19:56:57 +0530

    learn dictionary comprehension

--- advancedpython.py
+++ advancedpython.py
@@ -535,6 +535,21 @@ def learnListComprehensions():
     oddsSquared = [ e**2 for e in odds if e>3 and e<17]
     print(oddsSquared)
 
+def learnDictComprehensions():
+    # define a list of temperature values
+    ctemps = [0, 12, 34, 100]
+    # use comprehension to build a dictionary
+    tempDict = {t: (t * 9/5) + 32 for t in ctemps if t <100}
+    print(tempDict)
+    print(tempDict[12])
+    # Merge two dictionaries with a comprehension
+    team1 = {"Jones": 24, "Jameson": 18, "Smith": 58, "Burns": 7}
+    team2 = {"White": 12, "Macke": 88, "Perce": 4}
+
+    newTeam = {k : v for team in (team1, team2) for k, v in team.items()}
+    print(newTeam)
+
+
 
 
     
@@ -564,7 +579,8 @@ if __name__ == "__main__":
     # learnComparisonOps()
     # learnBasicLogging()
     # learnCustomLogging()
-    learnListComprehensions()
+    # learnListComprehensions()
+    learnDictComprehensions()
 
 
 
