2c174c6117f93be9830c00f55bb1e7de46f78483
Jerrin Francis <jerrinfrancis7@gmail.com>
2020-01-05 19:23:16 +0530

    learn custom logging

--- advancedpython.py
+++ advancedpython.py
@@ -503,7 +503,22 @@ def learnBasicLogging():
     # Output formatted string to the log
     logging.info("Here's a {} variable and an int: {}".format("string", 10))
 
+extData = {'user': 'joem@example.com'}
+def anotherFunction():
+    logging.debug("This is a debug-level log message", extra=extData)
+def learnCustomLogging():
+    # set the output file and debug level, and
+    # use a custom formatting specification
+    fmtStr = "%(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d User:%(user)s %(message)s"
+    dateStr = "%m/%d/%Y %I:%M:%S %p"
+    logging.basicConfig(filename="output.log",
+                        level=logging.DEBUG,
+                        format=fmtStr,
+                        datefmt=dateStr)
 
+    logging.info("This is an info-level log message", extra=extData)
+    logging.warning("This is a warning-level message", extra=extData)
+    anotherFunction()
 
     
 
@@ -530,7 +545,8 @@ if __name__ == "__main__":
     # learnComputedAttr()
     # learnObjectOps()
     # learnComparisonOps()
-    learnBasicLogging()
+    # learnBasicLogging()
+    learnCustomLogging()
 
 
 
