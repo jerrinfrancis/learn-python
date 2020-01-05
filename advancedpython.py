5c13aeb3217f57dbbec5c626742d388836ab7971
Jerrin Francis <jerrinfrancis7@gmail.com>
2020-01-05 18:48:25 +0530

    learning object operations

--- advancedpython.py
+++ advancedpython.py
@@ -414,6 +414,37 @@ def learnComputedAttr():
     print(cls1.red)
     # check dir functionality
     print(dir(cls1))
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        return f'<Point x:{self.x}, y:{self.y}>'
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+def learnObjectOps():
+    p1 = Point(10, 20)
+    p2 = Point(20, 30)
+    print(p1, p2)
+    # Add two points
+    # this will result in error of __add__ is not implemented
+
+    p3 = p1 + p2
+    print(p3)
+
+    p4 = p3 - p2
+    print(p4)
+
+    p1 += p2
+    print(p1)
+
+
 
     
 
@@ -437,6 +468,7 @@ if __name__ == "__main__":
     # learnCounter()
     # learnEnum()
     # learnStringReprOfObjects()
-    learnComputedAttr()
+    # learnComputedAttr()
+    learnObjectOps()
 
 
