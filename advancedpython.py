28c702513d508642a2115c8a657f2c2448285e68
Jerrin Francis <jerrinfrancis7@gmail.com>
2020-01-05 19:49:31 +0530

    learn list comprehensions

--- advancedpython.py
+++ advancedpython.py
@@ -520,6 +520,23 @@ def learnCustomLogging():
     logging.warning("This is a warning-level message", extra=extData)
     anotherFunction()
 
+def learnListComprehensions():
+    # define two lists of numbers
+    evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+    odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+    # perform a mapping and filter 
+    # evenSquared = list(
+    #     map(lambda e: e**2, filter(lambda e: e>4 and e<16,evens)))
+    # print(evenSquared)
+    #  using comprehensions
+    evenSquared = [ e**2 for e in evens]
+    print(evenSquared)
+
+    oddsSquared = [ e**2 for e in odds if e>3 and e<17]
+    print(oddsSquared)
+
+
+
     
 
 
@@ -546,7 +563,8 @@ if __name__ == "__main__":
     # learnObjectOps()
     # learnComparisonOps()
     # learnBasicLogging()
-    learnCustomLogging()
+    # learnCustomLogging()
+    learnListComprehensions()
 
 
 
