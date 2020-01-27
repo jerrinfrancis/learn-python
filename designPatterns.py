04542a22cf8a8c01c13a0c12ca24d6f756ce7713
Jerrin Francis <jerrinfrancis7@gmail.com>
2020-01-27 23:27:06 +0530

    Learning Advanced design Patterns

--- designPatterns.py
+++ designPatterns.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import sys
-
+import pickle
 
 class SubsystemA:
     def method1(self):
@@ -162,9 +162,133 @@ def mediatorPattern():
 
     c1.send("Good Morning!")
 
+class Originator:
+    def __init__(self):
+        self._state = None
+    def create_memento(self):
+        return pickle.dumps(vars(self))
+    
+    def set_memento(self, memento):
+        previous_state = pickle.loads(memento)
+        vars(self).clear()
+        vars(self).update(previous_state)
+
+
+def mementoPattern():
+    originator = Originator()
+    
+    print(vars(originator))
+
+    memento = originator.create_memento()
+
+    originator._state = True
+
+    print(vars(originator))
+
+    originator.set_memento(memento)
+
+    print(vars(originator))
+class AtmState():
+    name = "state"
+    allowed = []
+    def goNext(self, state):
+        if state.name in self.allowed:
+            print(f'Current state: {self} switched on to: {state.name}  ')
+            self.__class__ = state
+        else:
+            print(f'Current state: {self} switched on to: {state.name} not possible')
+
+    def __str__(self):
+        return self.name
+class Off(AtmState):
+    name = "off"
+    allowed = ['on']
+class On(AtmState):
+    name = "on"
+    allowed = ['off']
+
+class ATM():
+    def __init__(self):
+        self.current = Off()
+    def setState(self, state):
+        self.current.goNext(state)
+
+
+def statePattern():
+    atm = ATM()
+    atm.setState(On)
+    atm.setState(On)
+    atm.setState(Off)
+
+class AbstractClass(ABC):
+#This class inherit from Abstract Base Class to allow the use of the @abstractmethod decorator
+    
+	def template_method(self):
+		"""Ths is the template method that contains a collection of 
+		methods to stay the same, to be overriden, and to be overriden optionally.
+		"""
+
+		self.__always_do_this()
+		self.do_step_1()
+		self.do_step_2()
+		self.do_this_or()
+
+	def __always_do_this(self):
+		#This is a protected method that should not be overriden.
+
+		name = sys._getframe().f_code.co_name
+		print('{}.{}'.format(self.__class__.__name__, name))
+
+	@abstractmethod
+	def do_step_1(self):
+		#This method should be overriden
+		pass
+
+	@abstractmethod
+	def do_step_2(self):
+		#This method should be overriden
+		pass
+
+	def do_this_or(self):
+		print('You can overide me but you do not have to')
+
+class ConcreteClassA(AbstractClass):
+#This class inherits from the Abstract class featuring the template method. 
+
+	def do_step_1(self):
+		print('Doing step 1 for ConcreteClassA ...')
+
+	def do_step_2(self):
+		print('Doing step 2 for ConcreteClassA ...')
+
+class ConcreteClassB(AbstractClass):
+#This class inherits from the Abstract class featuring the template method.
+
+	def do_step_1(self):
+		print('Doing step 1 for ConcreteClassB ...')
+
+	def do_step_2(self):
+		print('Doing step 2 for ConcreteClassB ...')
+
+	def do_this_or(self):
+		print('Doing my own business ...')
+
+def templatePattern():
+	print('==ConcreteClassA==')
+	a = ConcreteClassA()
+	a.template_method()
+
+	print('==ConcreteClassB==')
+	b = ConcreteClassB()
+	b.template_method()
+
+
 
 if __name__ == "__main__":
     # facade()
     # commandPattern()
     # interpreterPattern()
-    mediatorPattern()
+    # mediatorPattern()
+    # mementoPattern()
+    # statePattern()
+    templatePattern()
